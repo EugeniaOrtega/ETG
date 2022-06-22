3da27dcfc6d0d30122d86ba59d28a6b93d89ec27
EugeniaOrtega <TfgEugeniaETG@gmail.com>
2022-06-22 20:41:01 +0200

    upgrade components

--- src/components/Home.js
+++ src/components/Home.js
@@ -80,7 +80,7 @@ export function Home() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start mr-60">
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start mx-auto">
                 <div className="flex-shrink-0 flex items-center">
                   <img
                     className="block lg:hidden h-8 w-auto "
@@ -113,80 +113,87 @@ export function Home() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 ml-10 ">
-                {/* Profile dropdown */}
-                <Menu as="div" className=" mt-2 mr-28 ">
-                  <div>
-                    <Menu.Button className=" flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white ml-6">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.photoURL || "user.png"}
-                        alt=""
-                      />
-                      <span className="py-2 mx-2 text-bla">
-                        {user.displayName || user.email}
-                      </span>
-                    </Menu.Button>
+
+              <div className="grid grid-cols-2">
+                <div className="order-1 col-span-1 pb-20 ">
+                  <div className="absolute inset-y-0 right-0 mx-auto  ">
+                    {/* Profile dropdown */}
+                    <Menu as="div" className=" mt-2 mx-auto ">
+                      <div>
+                        <Menu.Button className=" flex text-sm rounded-full    ">
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src={user.photoURL || "user.png"}
+                            alt=""
+                          />
+                          <span className="py-2 mx-2 text-bla">
+                            {user.displayName || user.email}
+                          </span>
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className=" right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 absolute  ">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="/profile"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Perfil
+                              </a>
+                            )}
+                          </Menu.Item>
+
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="/#"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                                onClick={handleLogout}
+                              >
+                                Cerrar sesion
+                              </a>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
                   </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none ">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="/profile"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Perfil
-                          </a>
-                        )}
-                      </Menu.Item>
+                </div>
 
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="/#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            onClick={handleLogout}
-                          >
-                            Cerrar sesion
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-
-              <div class="flex my-1 text-black mx-4">
-                {isReadyForInstall && (
-                  <a
-                    className="mx-2 ml-3 my-4 mt-5 "
-                    href="/##"
-                    onClick={installPwa}
-                  >
-                    <img
-                      class="object-cover w-10 h-10 mt-4 border-4 border-blue-600 rounded-full inline-block my-4 mr-2 "
-                      src="smartphone.png"
-                      alt=""
-                    />
-                    App
-                  </a>
-                )}
+                <div className="order-2 md:order-3 col-span-full md:col-span-1 py-5 md:py-10 px-6 ">
+                  <div class="flex my-1 text-black mx-4 mx-auto">
+                    {isReadyForInstall && (
+                      <a
+                        className="mx-2 ml-3 my-4 mt-5 "
+                        href="/##"
+                        onClick={installPwa}
+                      >
+                        <img
+                          class="object-cover w-10 h-10 mt-4 border-4 border-blue-600 rounded-full inline-block my-4 mr-2 "
+                          src="smartphone.png"
+                          alt=""
+                        />
+                        App
+                      </a>
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/components/PreguntasFrecuentes.js
+++ src/components/PreguntasFrecuentes.js
@@ -5,57 +5,55 @@ export function Questions() {
   return (
     <div className="w-full bg-[url('/public/vessel.png')]">
       <Home />{" "}
-      <div className="mt-10 bg-blue-200 w-full max-w-screen-lg mx-auto">
-        <div className="md:grid md:grid-cols-1 ">
+      <div className="mt-12 bg-blue-200 w-full max-w-screen-sm mx-auto">
+        <div className="md:grid md:grid-cols-1 bg-blue-200 w-full max-w-screen-lg mx-auto ">
           <Fade right>
-            <div class="w-full ">
-              <div class="w-full max-w-screen-lg px-10 py-8 mx-auto bg-blue-200 rounded-lg shadow-xl">
-                <h1 class="text-xl mb-10 text-center">Preguntas Frecuentes</h1>
+            <div class="w-auto max-w-screen-lg px-10 py-8 mx-auto bg-blue-200 rounded-lg shadow-xl">
+              <h1 class="text-xl mb-2 text-center">Preguntas Frecuentes</h1>
 
-                <details class="w-full bg-white border border-blue-500 cursor-pointer mb-3">
-                  <summary class="w-full bg-white text-dark flex justify-between px-4 py-3  after:content-['+']">
-                    ¿Que es ETG?
-                  </summary>
-                  <Fade bottom>
-                    <p class="px-4 py-3 ">
-                      ETG se ha convertido en un jugador global con una cartera
-                      diversa de experiencia en múltiples industrias, que abarca
-                      insumos agrícolas, logística, comercialización y
-                      procesamiento, optimización de la cadena de suministro,
-                      transformación digital y energía.
-                    </p>
-                  </Fade>
-                </details>
+              <details class="w-full bg-white border border-blue-500 cursor-pointer mb-3">
+                <summary class="w-full bg-white text-dark flex justify-between px-4 py-3  after:content-['+']">
+                  ¿Que es ETG?
+                </summary>
+                <Fade bottom>
+                  <p class="px-4 py-3 ">
+                    ETG se ha convertido en un jugador global con una cartera
+                    diversa de experiencia en múltiples industrias, que abarca
+                    insumos agrícolas, logística, comercialización y
+                    procesamiento, optimización de la cadena de suministro,
+                    transformación digital y energía.
+                  </p>
+                </Fade>
+              </details>
 
-                <details class="w-full bg-white border border-blue-500 cursor-pointer mb-3">
-                  <summary class="w-full bg-white text-dark flex justify-between px-4 py-3 after:content-['+']">
-                    ¿Que hacemos en ETG?
-                  </summary>
-                  <Fade left>
-                    <p class="px-4 py-3">
-                      ETG visualiza un mundo donde transcendemos las
-                      limitaciones,mientras que a su vez impactamos
-                      positivamente en la vida de todos nuestros socios
-                    </p>
-                  </Fade>
-                </details>
+              <details class="w-full bg-white border border-blue-500 cursor-pointer mb-3">
+                <summary class="w-full bg-white text-dark flex justify-between px-4 py-3 after:content-['+']">
+                  ¿Que hacemos en ETG?
+                </summary>
+                <Fade left>
+                  <p class="px-4 py-3">
+                    ETG visualiza un mundo donde transcendemos las
+                    limitaciones,mientras que a su vez impactamos positivamente
+                    en la vida de todos nuestros socios
+                  </p>
+                </Fade>
+              </details>
 
-                <details class="w-full bg-white border border-blue-500 cursor-pointer mb-3">
-                  <summary class="w-full bg-white text-dark flex justify-between px-4 py-3  after:content-['+']">
-                    ¿Cual es el objetivo de ETG?
-                  </summary>
-                  <Fade right>
-                    <p class="px-4 py-3">
-                      ¡El espiritu emprendedor está vivo en ETG!{" "}
-                    </p>
-                    <p class="px-4 py-3">
-                      Nos adaptamos y crecemos responsablemente como una empresa
-                      diversificada,creadno un futuro sostenible para todos
-                      nuestros grupos de interés.
-                    </p>
-                  </Fade>
-                </details>
-              </div>
+              <details class="w-full bg-white border border-blue-500 cursor-pointer mb-3">
+                <summary class="w-full bg-white text-dark flex justify-between px-4 py-3  after:content-['+']">
+                  ¿Cual es el objetivo de ETG?
+                </summary>
+                <Fade right>
+                  <p class="px-4 py-3">
+                    ¡El espiritu emprendedor está vivo en ETG!{" "}
+                  </p>
+                  <p class="px-4 py-3">
+                    Nos adaptamos y crecemos responsablemente como una empresa
+                    diversificada,creadno un futuro sostenible para todos
+                    nuestros grupos de interés.
+                  </p>
+                </Fade>
+              </details>
             </div>
           </Fade>
         </div>
